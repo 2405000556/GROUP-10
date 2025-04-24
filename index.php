bdbf5751a2d2356716f3e8c14e6eb741456ea283
2405000556 <scottivanm12@gmail.com>
2025-04-24 09:19:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Yvan MUGABO PHP</title>
+</head>
+<body>
+    <h1>Yvan MUGABO PHP</h1>
+
+    <pre>
+    Y   Y
+     Y Y
+      Y
+      Y
+      Y
+    </pre>
+
+    <p>SHA256 of Yvan MUGABO:</p>
+    <p>
+        <?php
+            print hash('sha256', 'Yvan MUGABO');
+        ?>
+    </p>
+    <a href="check.php">click here to check the error setting</a><br>
+    <a href="fail.php">click here to cause a traceback</a>
+</body>
+</html>
